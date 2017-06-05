644207b2e4f4983f0fc5f9452c2b74b623e4dd3d
José Nascimento <joseaugustodearaujonascimento@gmail.com>
2017-06-05 12:43:32 -0300

    adicionada implementação de cliente com PHP

---
+++ tads-dsd-rest/tads-dsd-rest-php-client/index.php
@@ -0,0 +1,24 @@
+<?php
+
+include 'src/autoload.php';
+
+use io\tads\dsd\rest\php\ApiClient;
+use io\tads\dsd\rest\php\Configuration;
+use io\tads\dsd\rest\php\stub\CalculadoraApi;
+
+$config = new Configuration();
+$config->setHost('http://localhost:9090');
+
+$client = new ApiClient($config);
+
+$calculadora = new CalculadoraApi($client);
+
+$a = $argv[1];
+$b = $argv[2];
+
+$r = $calculadora->soma($a, $b);
+
+printf("%s + %s = %s\n", $a, $b, $r);
+
+var_dump($calculadora->soma($a, $b));
+
